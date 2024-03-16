4df7ac93df4d45afd3b5a7fa83da9ed33ee907de
Savio <01fe22bca081@kletech.ac.in>
2024-03-16 09:25:02 +0530

    theme.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "./theme";
+import { themeSettings } from "./themeBak";
 
 function App() {
   const mode = useSelector((state) => state.mode);
